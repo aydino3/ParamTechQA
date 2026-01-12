28fb7c034f34bb7773d03af9b44ff715e25c9652
aydino3 <aydino1996@gmail.com>
2026-01-13 00:14:23 +0300

    done

--- src/test/java/com/paramtech/runner/RunCucumberTest.java
+++ src/test/java/com/paramtech/runner/RunCucumberTest.java
@@ -5,12 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.paramtech.steps,com.paramtech.hooks")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ui")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:target/cucumber-report.html,json:target/cucumber.json")
+
 public class RunCucumberTest { }
